74b8a35861814d730dfa72c9dd7073dc68ebe49b
Donal-Noye <leekeiz49@gmail.com>
2026-03-15 23:47:26 +0400

    add simple connection postgresql

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"check-list/db"
 	"check-list/http"
 	"check-list/todo"
 	"fmt"
 )
 
 func main() {
+	db.CheckConnection()
+
 	todoList := todo.GetList()
 	httpHandlers := http.NewHTTPHandlers(todoList)
 	httpServer := http.NewHTTPServer(httpHandlers)
